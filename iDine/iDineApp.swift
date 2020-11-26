32399a34c929f8f24e11424f5cb1237cf9442a36
Rick Brown <ricky.brown.00@gmail.com>
2020-11-26 23:09:43 +0000

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Rick Brown on 26/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
